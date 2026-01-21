46eaf97b95fb0efd9a37e61eacd42af499b4c7e9
Josteena <josteena1224jose@gmail.com>
2026-01-21 13:04:22 +0530

    feat: Enhance product management interface and add stock log functionality

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -3,15 +3,15 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Product;
+use App\Entity\StockLog; 
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField; 
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 
 class ProductCrudController extends AbstractCrudController
 {
@@ -32,10 +32,11 @@ class ProductCrudController extends AbstractCrudController
     public function configureResponseParameters(KeyValueStore $responseParameters): KeyValueStore 
     {
         $em = $this->container->get('doctrine')->getManager();
-        $responseParameters->set(
-            'products',
-            $em->getRepository(Product::class)->findAll()
-        );
+      
+        $responseParameters->set('products', $em->getRepository(Product::class)->findAll());
+
+      
+        $responseParameters->set('history', $em->getRepository(StockLog::class)->findAll());
 
         return parent::configureResponseParameters($responseParameters);
     }
@@ -43,29 +44,11 @@ class ProductCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id', 'ID')
-                ->hideOnForm()
-                ->setColumns('col-md-1'),
-
-            TextField::new('name', 'Product Name')
-                ->setColumns('col-md-3'),
-
-            AssociationField::new('subCategory', 'Sub Category')
-                ->setColumns('col-md-3')
-                ->setRequired(true),
-
-            MoneyField::new('price', 'Price')
-                ->setCurrency('INR')
-                ->setStoredAsCents(false)
-                ->setColumns('col-md-2'),
-
-            IntegerField::new('stock', 'Stock Quantity')
-                ->setColumns('col-md-2'),
-
-            CollectionField::new('attributeValues', 'Product Variants')
-                ->useEntryCrudForm(ProductAttributeValueCrudController::class) 
-                ->setColumns('col-md-4')
-                ->hideOnIndex(),  
+            IdField::new('id', 'ID')->hideOnForm(),
+            TextField::new('name', 'Product Name'),
+            AssociationField::new('subCategory', 'Sub Category'),
+            MoneyField::new('price', 'Price')->setCurrency('INR')->setStoredAsCents(false),
+            IntegerField::new('stock', 'Stock Quantity'),
         ];
     }
 }
